1564a0d321cf44a81606616cdb9dc978e1aace6f
Robert151003 <148344123+Robert151003@users.noreply.github.com>
2024-08-17 22:42:28 +0100

    Flying Enemy

--- Assets/Scripts/hitpointValue.cs
+++ Assets/Scripts/hitpointValue.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class hitpointValue : MonoBehaviour
 {
     public int hitpoints;
+    public bool bullet;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (bullet)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
