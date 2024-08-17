7d478d775dac9fbba64c12f19906b6339a1d43be
Robert151003 <148344123+Robert151003@users.noreply.github.com>
2024-08-18 00:10:18 +0100

    Update BouncyBlock.cs

--- Assets/BouncyBlock.cs
+++ Assets/BouncyBlock.cs
@@ -8,14 +8,12 @@ public class BouncyBlock : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        // Check if the colliding object has a Rigidbody2D component
+
         Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            // Calculate the opposite direction of the current velocity
             Vector2 bounceDirection = -other.relativeVelocity.normalized;
 
-            // Apply force in the opposite direction
             rb.AddForce(bounceDirection * bounciness, ForceMode2D.Impulse);
         }
     }
