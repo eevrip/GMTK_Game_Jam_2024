934177dece0f0aa9148dc1f0de1deefee0820b3c
Robert151003 <148344123+Robert151003@users.noreply.github.com>
2024-08-20 14:28:14 +0100

    Portal sounds

--- Assets/Scripts/PortalToNextLevel.cs
+++ Assets/Scripts/PortalToNextLevel.cs
@@ -8,16 +8,24 @@ public class PortalToNextLevel : MonoBehaviour
     public GameObject Manager;
     public GameObject fade;
     public int levelNum;
+    public AudioSource playerEnter;
+    public AudioSource loop;
+    public GameObject player;
+    public float maxVolumeDistance = 10f;
+    public float minVolumeDistance = 2f;
 
     private void Start()
     {
         Manager = GameObject.FindGameObjectWithTag("Manager");
+        player = GameObject.FindGameObjectWithTag("Player");
+        loop.Play();
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-           
+            loop.Stop();
+            playerEnter.Play();
             Manager.GetComponent<manager>().levels[levelNum] = true;
             Manager.GetComponent<manager>().SaveManager();
 
@@ -36,5 +44,18 @@ public class PortalToNextLevel : MonoBehaviour
             SceneManager.LoadScene(nextIdx);
         else
             SceneManager.LoadScene(0);
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        AdjustFootstepVolume(distanceToPlayer);
+    }
+
+    private void AdjustFootstepVolume(float distanceToPlayer)
+    {
+        float volume = Mathf.InverseLerp(maxVolumeDistance, minVolumeDistance, distanceToPlayer);
+        loop.volume = Mathf.Clamp(volume, 0f, 1f);
     }
 }
